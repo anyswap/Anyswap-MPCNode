fd721eff032aab8c4c91ee12ca53c6d66a07dd56
weijun-sh <huangweijun0322@163.com>
2020-11-17 20:39:56 +0800

    update version to 6.0.0

--- internal/params/version.go
+++ internal/params/version.go
@@ -21,9 +21,9 @@ import (
 )
 
 const (
-	VersionMajor = 5          // Major version component of the current release
-	VersionMinor = 2          // Minor version component of the current release
-	VersionPatch = 2         // Patch version component of the current release
+	VersionMajor = 6          // Major version component of the current release
+	VersionMinor = 0          // Minor version component of the current release
+	VersionPatch = 0         // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
@@ -73,9 +73,9 @@ var (
 )
 
 func SetVersion(version, commit, date string) {
-	fmt.Printf("gitVersion: %v\n", version)
-	fmt.Printf("gitCommit: %v\n", commit)
-	fmt.Printf("gitDate: %v\n", date)
+	fmt.Printf("\ngitVersion: %v\n", version)
+	fmt.Printf("gitCommit   : %v\n", commit)
+	fmt.Printf("gitDate     : %v\n\n", date)
 	gitVersion = version
 	gitCommit = commit
 	gitDate = date
